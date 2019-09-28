a85720de250681b0f22c508d4585111ff23d7eb7
cerenkocaogullar <ceren@valensas.com>
2019-09-28 20:37:00 +0300

    Content change and style fix in notification page

--- src/app/notifications/notifications.page.ts
+++ src/app/notifications/notifications.page.ts
@@ -14,44 +14,44 @@ export class NotificationsPage implements OnInit {
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text: 'Mısra Şov Yaptı!',
+        title: 'Yeni pozisyonun için tebrik ederim!',
+        text: 'Gül Polat seni kutladı',
         type: NotificationType.Coworker
       },
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text: 'Mısra Çok Mutlu :)',
+        title: 'Ürün güzel paketlenmişti. Emeği gelen herkese teşekkürler',
+        text: 'Müşteri teşekkürü',
         type: NotificationType.Customer
       },
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text: 'Mısra Şov Yaptı!',
-        type: NotificationType.Coworker
+        title:
+          'İrem Canlı Modanisa’daki 2. yılını kutluyor! Ona iyi dileklerini iletmek için tıkla.',
+        text: 'Modanisa bildirimi',
+        type: NotificationType.System
       },
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text:
-          'Mısra Çok Mutlu :) Baya iyi fazla mutlu, aousgodsufosudfhoıdshoıdsf, afdoguhdofsıughıpofd \n ausgdsaud',
+        title: 'Ürünüm zamanında elime ulaştı. Teşekkürler Modanisa!',
+        text: 'Müşteri teşekkürü',
         type: NotificationType.Customer
       },
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text: 'Mısra Şov Yaptı!',
+        title: 'Bu haftaki hedefini aşan Karanfil takımını tebrik ederim.',
+        text: 'Yönetici tebriği',
         type: NotificationType.Supervisor
       },
       {
         date: new Date(),
         image: 'https://i1.sndcdn.com/avatars-000045137497-nsoqu4-t500x500.jpg',
-        title: 'Mısra Şov Yaptı!',
-        text: 'Mısra Çok Mutlu :)',
+        title: 'Tebrikler çaylak, bir sonraki lige çok yaklaştın, hadi! ',
+        text: 'Modanisa bildirimi',
         type: NotificationType.System
       }
     ];
