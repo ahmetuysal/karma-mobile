9841b41328f004b9a5b9f8b98411853524758145
Ahmet Uysal <auysal16@ku.edu.tr>
2019-09-29 02:34:25 +0300

    Implemented leaderboard page

--- src/app/individual-leaderboard/individual-leaderboard.page.ts
+++ src/app/individual-leaderboard/individual-leaderboard.page.ts
@@ -3,13 +3,79 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-individual-leaderboard',
   templateUrl: './individual-leaderboard.page.html',
-  styleUrls: ['./individual-leaderboard.page.scss'],
+  styleUrls: ['./individual-leaderboard.page.scss']
 })
 export class IndividualLeaderboardPage implements OnInit {
+  goldRest: { name: string; exp: number }[];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.goldRest = [
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      },
+      {
+        name: 'Ahmet Uysal',
+        exp: 12312
+      }
+    ];
   }
-
 }
