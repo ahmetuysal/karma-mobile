523016c60727283a9347635d4cbd087b80e5844a
cerenkocaogullar <ceren@valensas.com>
2019-09-29 04:00:29 +0300

    Content fixes

--- src/app/individual-leaderboard/individual-leaderboard.page.ts
+++ src/app/individual-leaderboard/individual-leaderboard.page.ts
@@ -17,64 +17,56 @@ export class IndividualLeaderboardPage implements OnInit {
         exp: 12312
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Yekta Kocaoğullar',
+        exp: 12000
+      },
+      {
+        name: 'Mısra Taşçı',
+        exp: 11050
+      },
+      {
+        name: 'Gül Sena Altıntaş',
+        exp: 9403
+      },
+      {
+        name: 'İrem Tarkın',
+        exp: 8700
+      },
+      {
+        name: 'Ceren Kocaoğullar',
+        exp: 8607
+      },
+      {
+        name: 'Ekin Berk Polat',
+        exp: 8003
       },
       {
         name: 'Ahmet Uysal',
-        exp: 12312
+        exp: 7065
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Yekta Kocaoğullar',
+        exp: 7000
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Mısra Taşçı',
+        exp: 6665
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Gül Sena Altıntaş',
+        exp: 6504
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'İrem Tarkın',
+        exp: 6230
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Ceren Kocaoğullar',
+        exp: 5004
       },
       {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
-      },
-      {
-        name: 'Ahmet Uysal',
-        exp: 12312
+        name: 'Ekin Berk Polat',
+        exp: 4300
       }
     ];
   }
--- src/app/leaderboard-team/leaderboard-team.page.ts
+++ src/app/leaderboard-team/leaderboard-team.page.ts
@@ -3,13 +3,70 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-leaderboard-team',
   templateUrl: './leaderboard-team.page.html',
-  styleUrls: ['./leaderboard-team.page.scss'],
+  styleUrls: ['./leaderboard-team.page.scss']
 })
 export class LeaderboardTeamPage implements OnInit {
-
-  constructor() { }
+  goldRest: { name: string; exp: number }[];
+  constructor() {}
 
   ngOnInit() {
+    this.goldRest = [
+      {
+        name: 'Su Samurları',
+        exp: 12312
+      },
+      {
+        name: 'Jimnastikçiler',
+        exp: 12000
+      },
+      {
+        name: 'Simit',
+        exp: 11050
+      },
+      {
+        name: 'Gül Sena ve Arkadaşları',
+        exp: 9403
+      },
+      {
+        name: 'Ekip #1',
+        exp: 8700
+      },
+      {
+        name: 'Şampiyonlar',
+        exp: 8607
+      },
+      {
+        name: 'Koş Takım Koş',
+        exp: 8003
+      },
+      {
+        name: 'Tayfa Maraton',
+        exp: 7065
+      },
+      {
+        name: 'Oyuncular',
+        exp: 7000
+      },
+      {
+        name: 'Kazanacak Takım',
+        exp: 6665
+      },
+      {
+        name: 'Kovalayıcılar',
+        exp: 6504
+      },
+      {
+        name: 'Kim Bilir Kimler',
+        exp: 6230
+      },
+      {
+        name: 'Oyunlaştırma Ekibi',
+        exp: 5004
+      },
+      {
+        name: 'Hırs Küpleri',
+        exp: 4300
+      }
+    ];
   }
-
 }
